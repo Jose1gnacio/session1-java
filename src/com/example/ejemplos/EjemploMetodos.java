55c26e87b85f82bd6574a725e720d5474fc42cff
Jose1gnacio <joseignacio.alvarez.n@gmail.com>
2024-04-02 21:29:41 -0300

    session 15 java

--- src/com/example/ejemplos/EjemploMetodos.java
+++ src/com/example/ejemplos/EjemploMetodos.java
@@ -1,5 +1,7 @@
 package com.example.ejemplos;
 
+import java.util.Random;
+
 public class EjemploMetodos {
     public static void main(String[] args) {
         String[] titleArray = new String[] {
@@ -11,9 +13,12 @@ public class EjemploMetodos {
                 "Troya",
                 "Ip man"
         };
-        String title = play(titleArray, 11);
+        String title = play(titleArray, 0);
+        String title2 = play(titleArray);
         System.out.println();
         pause(title);
+        System.out.println();
+        pause(title2);
     }
     public static String play(String[] titleArray, int index) {
         if (index < titleArray.length) {
@@ -29,6 +34,16 @@ public class EjemploMetodos {
         }
 
     }
+    public static String play(String[] titleArray) {
+        Random numRandom = new Random();
+        int index = numRandom.nextInt(titleArray.length-1);
+        String title = titleArray[index];
+            for ( int i=0; i < 2; i++) {
+                System.out.println("Playing movie " + title);
+            }
+            return title;
+    }
+
     public static void pause(String title) {
         if (title.isEmpty()) {
             System.out.println("No movie playing");
